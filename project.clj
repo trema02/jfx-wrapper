e4d72fd03479e3978ee6b207a413381302c0a987
Martin Treichel <martin.treichel@gmx.de>
2020-12-01 08:28:12 +0100

    MIT-License

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject de.trema02/jfx-wrapper "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT-License"
+            :url "https://opensource.org/licenses/mit-license.php" ; this is going to break
+            :distribution :repo}
   :aot :all
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [proto-repl "0.3.1"]]
